ba7693b7f35fc8c0863780b86b5261c379d491c4
jenishthanki <jenishhd@gmail.com>
2020-05-22 15:13:55 -0700

    Music Database practice

---
+++ src/model/DataSource.java
@@ -0,0 +1,14 @@
+package model;
+
+public class DataSource {
+    public static final String DB_NAME = "music.db";
+    public static final String CONNECTION_STRING = "jbdc:sqlite:/Users/jenishthanki/IdeaProjects/MusicDatabase/"+DB_NAME;
+
+    public static final String TABLE_ALBUM = "albums";
+    public static final String COLUMN_ALBUM_ID = "_id";
+    public static final String COLUMN_ALBUM_NAME = "name";
+    public static final String COLUMN_ALBUM_ARTIST = "artists";
+
+    public
+
+}
